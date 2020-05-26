a742b9971ae0ac4f60db9060ff9149919a0f372a
Eleni Christoforidou <christoforidoueleni@gmail.com>
2020-05-26 19:18:46 +0100

    Add files via upload

--- plotReadQualityHist.m
+++ plotReadQualityHist.m
@@ -28,7 +28,7 @@ data4=readmatrix('NanoPlot-data_barcode10.txt'); %load data.
 data5=readmatrix('NanoPlot-data_barcode11.txt'); %load data.
 data6=readmatrix('NanoPlot-data_barcode12.txt'); %load data.
 
-%Supblot 1.
+%Subplot 1.
 fig=figure('Position', get(0, 'Screensize')); %make figure full-screen.
 ax(1)=subplot(2,3,1);
 h1=histogram(data1(:,1));
@@ -40,7 +40,7 @@ h1.FaceColor=[.5 .8 .7]; %change bar fill colour.
 h1.EdgeColor=[.5 .8 .7]; %change bar outline colour.
 line([7, 7], [0 30000], 'LineWidth', 2, 'Color', 'b'); %add line showing quality cut-off.
 
-%Supblot 2.
+%Subplot 2.
 ax(2)=subplot(2,3,2);
 h2=histogram(data2(:,1));
 ylabel('Number of reads','FontSize',12); %add y-axis label.
@@ -51,7 +51,7 @@ h2.FaceColor=[.3 .6 .5]; %change bar fill colour.
 h2.EdgeColor=[.3 .6 .5]; %change bar outline colour.
 line([7, 7], [0 30000], 'LineWidth', 2, 'Color', 'b'); %add line showing quality cut-off.
 
-%Supblot 3.
+%Subplot 3.
 ax(3)=subplot(2,3,3);
 h3=histogram(data3(:,1));
 ylabel('Number of reads','FontSize',12); %add y-axis label.
@@ -62,7 +62,7 @@ h3.FaceColor=[.1 .4 .3]; %change bar fill colour.
 h3.EdgeColor=[.1 .4 .3]; %change bar outline colour.
 line([7, 7], [0 30000], 'LineWidth', 2, 'Color', 'b'); %add line showing quality cut-off.
 
-%Supblot 4.
+%Subplot 4.
 ax(4)=subplot(2,3,4);
 h4=histogram(data4(:,1));
 ylabel('Number of reads','FontSize',12); %add y-axis label.
@@ -73,7 +73,7 @@ h4.FaceColor=[.8 .5 .7]; %change bar fill colour.
 h4.EdgeColor=[.8 .5 .7]; %change bar outline colour.
 line([7, 7], [0 30000], 'LineWidth', 2, 'Color', 'b'); %add line showing quality cut-off.
 
-%Supblot 5.
+%Subplot 5.
 ax(5)=subplot(2,3,5);
 h5=histogram(data5(:,1));
 ylabel('Number of reads','FontSize',12); %add y-axis label.
@@ -84,7 +84,7 @@ h5.FaceColor=[.6 .3 .5]; %change bar fill colour.
 h5.EdgeColor=[.6 .3 .5]; %change bar outline colour.
 line([7, 7], [0 30000], 'LineWidth', 2, 'Color', 'b'); %add line showing quality cut-off.
 
-%Supblot 6.
+%Subplot 6.
 ax(6)=subplot(2,3,6);
 h6=histogram(data6(:,1));
 ylabel('Number of reads','FontSize',12); %add y-axis label.
